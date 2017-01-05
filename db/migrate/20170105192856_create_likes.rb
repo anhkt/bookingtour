7e37f61e24ed9cc966df12e8632de3c82c34897b
anhkt <anhkt1603@gmail.com>
2017-01-06 02:43:17 +0700

    init model

---
+++ db/migrate/20170105192856_create_likes.rb
@@ -0,0 +1,12 @@
+class CreateLikes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :likes do |t|
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :likeable, polymophic: true
+      t.datetime :deleted_at
+
+      t.timestamps
+    end
+      add_index :likes, [:likeable_id, :likeable_type]
+  end
+end
